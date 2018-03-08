5e128f0213a29b78b0fd33b425a61c4c9870ac38
Elizabeth Nelson <17551572+ElizabethLane@users.noreply.github.com>
2018-03-08 08:37:28 -0800

    activity main xml had two button views added (increment and decrement). Main activity java file now has two methods - increment and decrement which display quantity

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -30,9 +30,25 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        int quantity = 2;
+        display(quantity);
+        displayPrice(quantity * 5);
+    }
 
-        display(77 * 2 + 1);
-        displayPrice(2 * 3);
+    /**
+     * This method is called when the + button is clocked.
+     */
+    public void increment(View view){
+        int quantity = 3;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the - button is clicked.
+     */
+    public void decrement(View view){
+        int quantity = 1;
+        display(quantity);
     }
 
     /**
