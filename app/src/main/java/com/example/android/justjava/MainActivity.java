44ab3f400cc77c586bb52bd35a47bd94f65832ca
Elizabeth Nelson <17551572+ElizabethLane@users.noreply.github.com>
2018-03-07 19:24:59 -0800

    Added price and order textviews.  Added displayprice method.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -29,7 +30,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(2 * 2);
+
+        display(77 * 2 + 1);
+        displayPrice(2 * 3);
     }
 
     /**
@@ -38,5 +41,15 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
