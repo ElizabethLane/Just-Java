4f178cd5eb5cb7172233d292ce73a876c0a4333f
Elizabeth Nelson <17551572+ElizabethLane@users.noreply.github.com>
2018-03-08 09:43:22 -0800

    Made quantity variable global in class main scope

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -26,11 +26,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    int quantity = 1;
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity = 2;
         display(quantity);
         displayPrice(quantity * 5);
     }
@@ -39,8 +39,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the + button is clocked.
      */
     public void increment(View view){
-        int quantity = 2;
-        quantity = 3;
+        quantity = quantity + 1;
         display(quantity);
     }
 
@@ -48,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the - button is clicked.
      */
     public void decrement(View view){
-        int quantity = 1;
+        quantity = quantity - 1;
         display(quantity);
     }
 
